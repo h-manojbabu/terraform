abd61afefeb39db6b034245ded057df0c41c70d7
Manoj <59554997+h-manojbabu@users.noreply.github.com>
2026-08-17 14:30:21 +0530

    Create providers.tf

---
+++ EKS_AKS_Deployment/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = local.common_tags
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+}
