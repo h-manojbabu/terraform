272e698c0da9df8e780c5a0b6ba3ba6ef41a126c
Manoj <59554997+h-manojbabu@users.noreply.github.com>
2026-08-17 14:25:29 +0530

    Create main.tf

---
+++ EKS_AKS_Deployment/moldule/azure-network/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.name_prefix}-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.vnet_address_space
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "${var.name_prefix}-aks-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.aks_subnet_prefixes
+}
